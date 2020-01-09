5371b3d025aa1aaaeaa9c2a700f600da8ca8129e
ekelund1 <jonathan.ekelund1@gmail.com>
2020-01-09 22:52:36 +0100

    Added the first test world and begun understanding and adding some logic to how the camera and different parts of unity operates.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform ballObject;
+    public float distanceFromBall = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookOnObject = ballObject.position - transform.position;
+        lookOnObject = ballObject.position - transform.position;
+        transform.forward = lookOnObject.normalized;
+
+        
+
+        distanceFromBall = Mathf.Max(ballObject.GetComponent<Rigidbody>().velocity.magnitude, 20f);
+
+        Vector3 playerLastPosition;
+        playerLastPosition = ballObject.position - lookOnObject.normalized * distanceFromBall;
+        transform.position = playerLastPosition;
+    }
+}
